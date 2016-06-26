b534a748a0cb0ad5baaebfc9b73377073d6762c8
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-26 14:56:33 -0400

    html pages being routed

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,19 +1,19 @@
 // Load Data
 // linking our routes to a series of data sources.
-var friend_data = require('../data/friend.js');
+var friends_data = require('../data/friends.js');
 
 
 
 // Routing
 module.exports = function(app) {
 	
-	// API get requests
+	// API GET requests
 	// link to the data stored in our app so that users can see the JSON data when they visit the below link
 	app.get('/api/friends', function(req, res) {
-		res.json(friend_data);
+		res.json(friends_data);
 	});
 
-	// API post requests
+	// API POST requests
 	// below handles the user submitted form. In this app's case, the friend data to match that user to others that are in the data js files.
 	app.post('api/friends', function(req, res) {
 		// body...
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,3 +1,21 @@
 // Dependancies
 // we need to include the path package to get the correct file path for our html
 var path = require('path');
+
+
+
+// Routing
+module.exports = function(app) {
+	
+	// HTML GET requests
+	// below is the code that handles when a user visits a page so that the appropriate html page is shown to the user
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// default home screen if no matching route is found
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, '/../public/home.html'));
+	});
+
+}
