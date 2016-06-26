04acdb0280932b91702420ff22860c89d087de53
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-26 16:22:13 -0400

    started on home.html

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -10,12 +10,12 @@ module.exports = function(app) {
 	// HTML GET requests
 	// below is the code that handles when a user visits a page so that the appropriate html page is shown to the user
 	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+		res.sendFile(path.join(__dirname, '..', 'public', 'survey.html'));
 	});
 
 	// default home screen if no matching route is found
 	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname, '/../public/home.html'));
+		res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
 	});
 
 }
