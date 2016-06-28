416476b8a8afcdf2721eef9c0ce0df7bde84338d
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-28 10:50:17 -0400

    range sliders now displaying values to screen

--- app/public/javascript/scripts.js
+++ app/public/javascript/scripts.js
@@ -1,10 +1,33 @@
 // Javascript function that wraps everything
 $(document).ready(function(){
 
-    $('#q1').mousemove(function() {
-    	
-    	$('#op-1').text($('#q1').val());
+	// Functions
 
-    });
+	// function that displays the value of each range slider on the page and is passed this from the mousemove event trigger
+	function rangeSlider(event) {
 
-});
+		// store the id of the input being used
+		var the_id = ($(event).attr('id'));
+
+		// split the id on the - and return the second index of the string array
+		var output_id = the_id.split('-')[1];
+
+		// display the value of the rangeslider to the output element
+		$('#op-' + output_id).text($('#' + the_id).val());
+
+	} // end rangeSlider()
+
+
+	// Events
+
+    $('.rangeslider').mousemove(function(e) {
+
+    	// store this in the event parameter
+    	e = this;
+
+    	// call the rangeslider function and pass this to it
+    	rangeSlider(e);
+
+    }); // end mousevove()
+
+}); // end jquery()
